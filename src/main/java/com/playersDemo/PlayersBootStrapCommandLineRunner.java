d41805c72abc43768174a865019e458bd67b5fa6
Nachi Veerappan Nachiappan <nachiap@apple.com>
2020-09-14 20:54:37 -0700

    Add files

--- src/main/java/com/playersDemo/PlayersBootStrapCommandLineRunner.java
+++ src/main/java/com/playersDemo/PlayersBootStrapCommandLineRunner.java
@@ -14,7 +14,7 @@ import java.io.File;
 import java.util.List;
 
 @Component
-class PlayerServiceCommandLineRunner implements CommandLineRunner {
+class PlayersBootStrapCommandLineRunner implements CommandLineRunner {
 
     @Autowired
     private PlayerRepository playerRepository;
